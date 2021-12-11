0ac528f6d930319da7a3b6c3ed59d1260cd655b5
upulsen <95944736+upulsen@users.noreply.github.com>
2021-12-11 15:39:37 +1100

    Update train.py
    slight change in random state to check the metrics difference

--- train.py
+++ train.py
@@ -40,7 +40,7 @@ le = LabelEncoder()
 y = le.fit_transform(y)
 
 #split the dataset into train and test set and train a random forest classifier on the train set. 
-X_train, X_test, y_train, y_test = train_test_split(df_x, y, test_size = 0.2, random_state = 42)
+X_train, X_test, y_train, y_test = train_test_split(df_x, y, test_size = 0.2, random_state = 45)
 clf2 = RandomForestClassifier(max_depth=10, random_state=0)
 clf2.fit(X_train, y_train)
 y_pred = clf2.predict(X_test)
